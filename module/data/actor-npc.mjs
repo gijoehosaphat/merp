a6aa96d5d2b5eb93322f5adfb5bb1bc16bc95349
Joe Leonard <joeleonard@gmail.com>
2024-11-04 14:47:31 -0400

    replace

---
+++ module/data/actor-npc.mjs
@@ -0,0 +1,19 @@
+import MERPActorBase from "./base-actor.mjs";
+
+export default class MERPNPC extends MERPActorBase {
+
+  static defineSchema() {
+    const fields = foundry.data.fields;
+    const requiredInteger = { required: true, nullable: false, integer: true };
+    const schema = super.defineSchema();
+
+    schema.cr = new fields.NumberField({ ...requiredInteger, initial: 1, min: 0 });
+    schema.xp = new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 });
+    
+    return schema
+  }
+
+  prepareDerivedData() {
+    this.xp = this.cr * this.cr * 100;
+  }
+}
---
+++ module/data/item-feature.mjs
@@ -0,0 +1,3 @@
+import MERPItemBase from "./base-item.mjs";
+
+export default class MERPFeature extends MERPItemBase {}
